928670253cdd625b409d92654033404458e64588
Marcos-Prado <marcosprprado@yahoo.com.br>
2021-08-02 17:21:23 -0300

    create method all for Priority success and failure

--- app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
@@ -4,17 +4,18 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.example.tasks.service.HeaderModel
+import com.example.tasks.service.model.HeaderModel
 import com.example.tasks.service.constants.TaskConstants
 import com.example.tasks.service.listener.APIListener
 import com.example.tasks.service.listener.ValidationListener
 import com.example.tasks.service.repository.PersonRepository
+import com.example.tasks.service.repository.PriorityRepository
 import com.example.tasks.service.repository.local.SecurityPreferences
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
     private val mPersonRepository = PersonRepository(application)
-
+    private val mPriorityRepository = PriorityRepository(application)
     private val mSharedPreferences = SecurityPreferences(application)
 
     private val mLogin = MutableLiveData<ValidationListener>()
@@ -50,6 +51,11 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
         val person = mSharedPreferences.get((TaskConstants.SHARED.PERSON_NAME))
 
         val logged = (token != "" && person != "")
+
+        if (!logged) {
+            mPriorityRepository.all()
+        }
+
         mLoggedUser.value = logged
     }
 
