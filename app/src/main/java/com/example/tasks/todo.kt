95c6d35b131f26db1363b21aace79e015422aae8
Marcos-Prado <marcosprprado@yahoo.com.br>
2021-08-02 17:36:24 -0300

    create todo for future refact

---
+++ app/src/main/java/com/example/tasks/todo.kt
@@ -0,0 +1,7 @@
+package com.example.tasks
+
+//TODO alterar todas chamadas do synthetics por view binding
+//TODO alterar todas chamadas de find view by id por view binding
+//TODO refatorar layouts
+//TODO refatorar métodos
+//TODO refatores indentações
